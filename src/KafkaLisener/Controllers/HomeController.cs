eb312721ff54749150ce27c2cdb143bd499ff712
Kirill Goncharov <kirill.myp@yandex.ru>
2020-09-09 23:51:03 +0300

    refactoring

--- src/KafkaLisener/Controllers/HomeController.cs
+++ src/KafkaLisener/Controllers/HomeController.cs
@@ -19,53 +19,5 @@ namespace KafkaLisener.Controllers
         {
             _listenerKafka.ListenMessage();
         }
-
-        public void Get2(string broker = "127.0.0.1", string topic = "quickstart-events-topic")//string brokerList, List<string> topics) //, CancellationToken cancellationToken)
-        {
-            var config = new ConsumerConfig
-            {
-                // the group.id property must be specified when creating a consumer, even 
-                // if you do not intend to use any consumer group functionality.
-                GroupId = new Guid().ToString(),
-                BootstrapServers = broker,
-                // partition offsets can be committed to a group even by consumers not
-                // subscribed to the group. in this example, auto commit is disabled
-                // to prevent this from occurring.
-                EnableAutoCommit = true
-            };
-
-            CancellationTokenSource cts = new CancellationTokenSource();
-
-            using (var consumer =
-                new ConsumerBuilder<Ignore, string>(config)
-                    .SetErrorHandler((_, e) => Console.WriteLine($"Error: {e.Reason}"))
-                    .Build())
-            {
-                consumer.Assign(new TopicPartitionOffset(topic, 0, Offset.Beginning));
-
-                try
-                {
-                    //while (true)
-                    //{
-                    try
-                    {
-                        //consumeResult.Offset - 
-
-                        var consumeResult = consumer.Consume(cts.Token);
-                        Console.WriteLine($"Received message at {consumeResult.TopicPartitionOffset}: ${consumeResult.Message.Value}");
-                    }
-                    catch (ConsumeException e)
-                    {
-                        Console.WriteLine($"Consume error: {e.Error.Reason}");
-                    }
-                    //}
-                }
-                catch (OperationCanceledException)
-                {
-                    Console.WriteLine("Closing consumer.");
-                    consumer.Close();
-                }
-            }
-        }
     }
 }
